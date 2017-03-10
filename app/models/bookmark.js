e012d33dfce807a2e80a97186ba912e6a8da14a9
mario <mario@uniplaces.com>
2017-03-10 17:53:29 +0000

    transform model

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    link: DS.attr('string'),
+    title: DS.attr('string'),
+    about: DS.attr('string'),
+    public: DS.attr('boolean'),
+    created: DS.attr('date', {
+        defaultValue() { return newDate(); }
+    })
 });
