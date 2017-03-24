cd9ad039eac56a518b32186e28be22f54ad8dd23
mario <mario@uniplaces.com>
2017-03-24 10:54:24 +0000

    add templates

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,26 +1,7 @@
 import Ember from 'ember';
 
-let bookmarks = [{
-    id: 1,
-    title: 'Microsoft',
-    link: 'https://www.microsoft.com',
-    about: 'At Microsof our mission and values are to help people and business'
-},
-{
-    id: 2,
-    title: 'Wikipedia',
-    link: 'https://pt.wikipedia.org/',
-    about: 'A enciclopédia livre que todos podem editar.'
-},
-{
-    id: 3,
-    title: 'Apple',
-    link: 'https://www.apple.com/',
-    about: null
-}
-];
 export default Ember.Route.extend({
     model() {
-        return bookmarks;
+        return this.get('store').findAll('bookmark');
     }
 });
