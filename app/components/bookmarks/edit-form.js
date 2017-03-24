03c883093f8f8267101ad0c0a91a2afe495b6c21
mario <mario@uniplaces.com>
2017-03-24 18:10:35 +0000

    add delete, create and edit actions to bookmarks

--- app/components/bookmarks/edit-form.js
+++ app/components/bookmarks/edit-form.js
@@ -1,8 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    bookmark: null,
+    router: Ember.inject.service('-routing'),
     actions: {
-        save(bookmark) { Ember.Logger.info('save cancelled');},
-        cancel() { Ember.Logger.info('cancel called');}
+        save(bookmark) { Ember.Logger.info('save called');
+        bookmark.save()
+        .then(
+            (value) => {
+                Ember.Logger.info(value);
+                this.get('router').transitionTo('bookmarks');
+            }
+        )
+        .catch(
+            () => {
+                Ember.Logger.info('failure to save');
+            }
+        )
+    },
+        cancel() { Ember.Logger.info('cancel called');},
+        delete(bookmark) {
+            bookmark.destroyRecord();
+            this.get('router').transitionTo('bookmarks');
+        }
     }
 });
